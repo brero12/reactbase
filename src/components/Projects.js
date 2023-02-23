7de32b5a4050530ccbad06360ac47cd599776c89
brero12 <8272332+brero12@users.noreply.github.com>
2023-02-22 20:48:52 -0800

    Adding unit test

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,20 +4,20 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-sky-900 body-font">
+    <section id="projects" title="project section" className="text-gray-400 bg-sky-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
       <div className="flex flex-col w-full mb-20">
         <VscCode className="mx-auto inline-block w-10 mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-lg">
+          <p  title="summary" className="lg:w-2/3 mx-auto leading-relaxed text-lg">
           I have worked with different services such as information systems, 
           SaaS applications and mobile or desktop applications. 
           You will see some of them below; for more information, <b> hover your mouse over them.</b>
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div title="project maps" className="flex flex-wrap -m-4">
           {projects.map((project) => (
             <a
               href={project.link}
